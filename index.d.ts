c7aeaa317865473489092f3fcd9c9fe77ea455b5
taqm <warabii.taqm@gmail.com>
2019-03-13 00:37:39 +0900

    Add types

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+interface Config {
+  apiKey: string;
+  databaseURL: string;
+  storageBucket: string;
+  authDomain: string;
+  messagingSenderId: string;
+  projectId: string;
+}
+
+const config: Config;
+export default config;
